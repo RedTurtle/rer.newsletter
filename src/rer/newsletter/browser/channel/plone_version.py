834ee774c57a74a5cdcf7cc8c53c015d47599d99
Mauro Amico <mauro.amico@gmail.com>
2023-07-20 21:05:53 +0200

    revert

--- src/rer/newsletter/browser/channel/plone_version.py
+++ src/rer/newsletter/browser/channel/plone_version.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from plone import api
-from plone.memoize import forever
+from plone.memoize import view
 from Products.Five.browser import BrowserView
 
 
 class PloneVersionView(BrowserView):
     """View to check Plone version"""
 
-    @forever.memoize
+    @view.memoize
     def is_plone_6_or_above(self):
         version = api.env.plone_version()
         major = version.split(".")[0]
