a99894dbc3137ce986a3af176663c5119342621c
Mauro Amico <mauro.amico@gmail.com>
2023-07-20 20:33:22 +0200

    fix test / memoize forever

--- src/rer/newsletter/browser/channel/plone_version.py
+++ src/rer/newsletter/browser/channel/plone_version.py
@@ -1,12 +1,13 @@
+# -*- coding: utf-8 -*-
 from plone import api
-from plone.memoize import view
+from plone.memoize import forever
 from Products.Five.browser import BrowserView
 
 
 class PloneVersionView(BrowserView):
     """View to check Plone version"""
 
-    @view.memoize
+    @forever.memoize
     def is_plone_6_or_above(self):
         version = api.env.plone_version()
         major = version.split(".")[0]
